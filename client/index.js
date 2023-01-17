3bd55ccdbd9be14bdecb8c8a663b180797352c9c
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-01-17 16:20:43 +0000

    Add form for adding goats

--- client/index.js
+++ client/index.js
@@ -1,4 +1,3 @@
-// const { render } = require("../server/app");
 
 const herd = document.getElementById("herd");
 
@@ -10,19 +9,61 @@ async function callTheHerd() {
     // Extracting the JSON data from the response
     const data = await res.json();
 
+    console.log(data)
+
     // Log them
     data.forEach(goat => {
-        const tr = document.createElement("tr")
-        const valueArray = []
-        let goatKeys = ['id', 'name', 'age', 'sex', 'favouriteColour']
-        for (key of goatKeys) {
-            const td = document.createElement("td")
-            td.textContent = goat[key]
-            tr.appendChild(td)
-        }
-        herd.appendChild(tr)
+        addGoatToTable(goat)
     })
-
 }
+
+function addGoatToTable(goat) {
+    const tr = document.createElement("tr");
+    let goatKeys = ['id', 'name', 'age', 'sex', 'favouriteColour'];
+    for (key of goatKeys) {
+        const td = document.createElement("td");
+        td.textContent = goat[key];
+        if (key === "favouriteColour") {
+            td.style.backgroundColor = goat[key];
+        }
+        tr.appendChild(td);
+    }
+    herd.appendChild(tr);
+}
+
+async function addGoat(goat) {
+    console.log(goat)
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(goat),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+    }
+
+    const res = await fetch("http://localhost:3000/goats", options)
+    const data = await res.json()
+    if (await res.status != 201) {
+        alert("Attempt to add goat failed")
+        return
+    } 
+    return await data
+}
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const goat = {
+        name: e.target.name.value,
+        age: e.target.age.value,
+        sex: e.target.sex.value,
+        favouriteColour: e.target.colour.value
+    };
+
+    goat = addGoat(goat)
+    addGoatToTable(goat)
+})
 
 callTheHerd()
